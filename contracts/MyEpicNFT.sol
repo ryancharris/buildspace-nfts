5242785ce52693960daad4222ae6d922c5cfcd75
Ryan Harris <harrisryan1@gmail.com>
2021-09-26 15:50:48 -0400

    finish lesson

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,9 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
-        console.log("This is my NFT contract. Woah!");
-    }
+contract MyEpicNFT is ERC721URIStorage {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor() ERC721 ("DopeNFT", "DOPE") {
+    console.log("This is my NFT contract. Woah!");
+  }
+
+  function makeAnEpicNFT() public {
+    uint256 newItemId = _tokenIds.current();
+
+    _safeMint(msg.sender, newItemId);
+
+    _setTokenURI(newItemId, "data:application/json;base64,ewogICJuYW1lIjogIkJpZyBEb2ciLAogICJkZXNjcmlwdGlvbiI6ICJBIGdvb2QgbG9va2luZyBkb2cgd2hvIGlzIHRpbnkiLAogICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStTR2xJWlhsSVpXeHNiend2ZEdWNGRENEtQQzl6ZG1jKyIKfQ==");
+    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+    _tokenIds.increment();
+  }
 }
